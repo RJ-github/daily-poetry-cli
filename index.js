22a96a165c37cd87a98598d3243c08facb3fc5a6
Roland <rolandlifejr@gmail.com>
2024-02-28 12:40:22 -0700

    Added bin folder and cmd folder

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("hello");
+const minimist = require('minimist');
+
+module.exports = () => {
+	const args = minimist(process.argv.slice(2));
+
+	const cmd = args._[0];
+
+	switch(cmd) {
+		case 'today':
+			require('./cmds/today')(args);
+			break;
+		default: {
+			console.error(`${cmd} is not a valid command`);
+			break;
+		}
+	}
+};
